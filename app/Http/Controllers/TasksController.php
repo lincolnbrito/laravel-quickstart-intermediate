dc431161241e8f2de9fe49c40742d70fb29accbe
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-07-20 11:46:15 -0300

    definindo middleware para tasks

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -8,6 +8,11 @@ use App\Http\Requests;
 
 class TasksController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         //
