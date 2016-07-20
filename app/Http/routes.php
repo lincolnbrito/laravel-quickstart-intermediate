3c2b537f1ffc16ca285f5673512f60eeb6b22655
Lincoln Brito <lincoln.sbrito@gmail.com>
2016-07-20 11:38:22 -0300

    criando rotas e controller para tasks

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/tasks', 'TasksController@index');
+Route::post('/tasks', 'TasksController@store');
+Route::delete('/tasks/{task}', 'TasksController@destroy');
